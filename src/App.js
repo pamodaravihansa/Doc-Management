a20ec80b9b34b151d01b585fe112312aeae067ce
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-10-28 18:31:43 +0530

    add login page illustration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Login from './components/auth/Login';
+
+
 
 function App() {
+  
   return (
+
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+    <Route path="/" element={<Login />} />
+    
+    </Routes>
     </div>
+    </Router>
   );
+  
 }
 
 export default App;
---
+++ src/components/layouts/PNavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './PNavBar.css'
+
+export default function
+    () {
+    return (
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand " href="#">
+                <img src="https://www.10qbit.com/assets/images/logo-inverted-new.png" width="100" height="30" class="d-inline-block align-top mr-3" alt="" />
+                Document Management System
+            </a>
+        </nav>
+    )
+}
+
